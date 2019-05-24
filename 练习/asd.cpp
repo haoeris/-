30dc0328988ce2b29011f809d592b5841a5a3122
haoeris <546278279@qq.com>
2019-05-24 11:48:10 +0800

    $ git config --global user.name "haoeris" $ git config --global user.email "546278279@qq.com"

---
+++ 练习/asd.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+static char* you = "米娅莫拉苏娜丹尼谢莉红";
+typedef char* myself;
+class I
+{
+public:
+	void loveyou(myself I)
+	{
+		cout<<I<<" love "<<you<<endl;
+		loveyou(I);
+	}
+};
+
+int main()
+{
+	I I;
+	myself i = "达拉崩吧班德贝蒂卜多比鲁翁";
+	I.loveyou(i);
+
+}
